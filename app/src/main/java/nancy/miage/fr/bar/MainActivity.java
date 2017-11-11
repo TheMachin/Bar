a1804a0d302765d00296946410b8f9dcfdd9cfc3
Florian Muller <muller.florian57@gmail.com>
2017-11-11 13:47:08 +0100

    Ajout Jeu d'essai Consumable

--- app/src/main/java/nancy/miage/fr/bar/MainActivity.java
+++ app/src/main/java/nancy/miage/fr/bar/MainActivity.java
@@ -1,13 +1,18 @@
 package nancy.miage.fr.bar;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.util.UUID;
+
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import nancy.miage.fr.bar.model.Consumable;
+import nancy.miage.fr.bar.model.TypeConsumable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Realm.init(getApplicationContext());
+
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .initialData(initialDataTransaction).build();
+        Realm.setDefaultConfiguration(realmConfiguration);
     }
 
     public void order(View view){
@@ -31,5 +41,45 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ListConsumable.class);
         startActivity(intent);
     }
+
+    private Realm.Transaction initialDataTransaction = new Realm.Transaction() {
+        @Override
+        public void execute(@NonNull Realm realm) {
+            Consumable consumable = realm.createObject(Consumable.class, UUID.randomUUID().toString());
+            consumable.setName("Coca Cola");
+            consumable.setType(TypeConsumable.BOISSON.name());
+            consumable.setPrice(2.50);
+
+            consumable = realm.createObject(Consumable.class, UUID.randomUUID().toString());
+            consumable.setName("Saucisson");
+            consumable.setType(TypeConsumable.NOURRITURE.name());
+            consumable.setPrice(2.50);
+
+            consumable = realm.createObject(Consumable.class, UUID.randomUUID().toString());
+            consumable.setName("Mojito");
+            consumable.setType(TypeConsumable.BOISSON.name());
+            consumable.setPrice(5.50);
+
+            consumable = realm.createObject(Consumable.class, UUID.randomUUID().toString());
+            consumable.setName("Diabolo banane");
+            consumable.setType(TypeConsumable.BOISSON.name());
+            consumable.setPrice(1.30);
+
+            consumable = realm.createObject(Consumable.class, UUID.randomUUID().toString());
+            consumable.setName("Glace à la menthe");
+            consumable.setType(TypeConsumable.DESSERT.name());
+            consumable.setPrice(3.00);
+
+            consumable = realm.createObject(Consumable.class, UUID.randomUUID().toString());
+            consumable.setName("Cacahuète");
+            consumable.setType(TypeConsumable.NOURRITURE.name());
+            consumable.setPrice(0.90);
+
+            consumable = realm.createObject(Consumable.class, UUID.randomUUID().toString());
+            consumable.setName("Ice Tea");
+            consumable.setType(TypeConsumable.BOISSON.name());
+            consumable.setPrice(1.70);
+        }
+    };
 
 }
